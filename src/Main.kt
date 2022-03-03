17975ab50acbc85c71a413cc6c3e3eb805755f7e
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-03-03 09:03:26 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+fun main(){
+ val studentkenyan:Boolean=true
+ println(studentkenyan)
+ var land:String="nyama kima"
+ println(land)
+ var cables:Double=3.98
+ println(cables)
+ val assigndoc:Boolean=false
+ println(assigndoc)
+ var name=arrayOf("jane","judy","grace")
+ println(name)
+ val age=arrayOf("30","60","20")
+ println(age)
+
+
+
+}
+
+
+
